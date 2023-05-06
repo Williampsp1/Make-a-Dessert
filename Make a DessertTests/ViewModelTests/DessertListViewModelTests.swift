4a9f267f724165c6ec9ecd3cbe5b7c030eb02c54
Williampsp1 <williampsp1@gmail.com>
2023-05-05 19:37:15 -0500

    Small test improvements

--- Make a DessertTests/ViewModelTests/DessertListViewModelTests.swift
+++ Make a DessertTests/ViewModelTests/DessertListViewModelTests.swift
@@ -13,15 +13,18 @@ final class DessertListViewModelTests: XCTestCase {
         let mockDessertMeals = [DessertListMealInfo(strMeal: "Apple Frangipan Tart", strMealThumb: "https://www.themealdb.com/images/media/meals/xqwwpy1483908697.jpg", id: "52768"), DessertListMealInfo(strMeal: "Blackberry Fool", strMealThumb: "https://www.themealdb.com/images/media/meals/xqwwpy1483908697.jpg", id: "52891")]
         let viewModel = DessertListViewModel(dessertProvider: MockDessertProvider())
         let task = await viewModel.getDessertMeals()
+        
         await task.value
         
         XCTAssertEqual(mockDessertMeals, viewModel.dessertListMeals)
+        XCTAssert(viewModel.errorMessage.isEmpty)
         XCTAssertFalse(viewModel.errorOccured)
     }
     
     func testDessertMealsInvalidURL() async {
         let viewModel = DessertListViewModel(dessertProvider: MockDessertProviderInvalidURL())
         let task = await viewModel.getDessertMeals()
+        
         await task.value
         
         XCTAssertEqual(DessertErrors.invalidURL.localizedDescription, viewModel.errorMessage)
@@ -32,6 +35,7 @@ final class DessertListViewModelTests: XCTestCase {
     func testDessertMealsNetworkError() async {
         let viewModel = DessertListViewModel(dessertProvider: MockDessertProviderNetworkError())
         let task = await viewModel.getDessertMeals()
+        
         await task.value
         
         XCTAssertEqual(DessertErrors.networkError.localizedDescription, viewModel.errorMessage)
@@ -42,6 +46,7 @@ final class DessertListViewModelTests: XCTestCase {
     func testDessertMealsDecodingError() async {
         let viewModel = DessertListViewModel(dessertProvider: MockDessertProviderDecodingError())
         let task = await viewModel.getDessertMeals()
+        
         await task.value
         
         XCTAssertEqual(DessertErrors.decodingError.localizedDescription, viewModel.errorMessage)
